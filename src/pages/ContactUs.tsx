b35d14fbccc8bbf6acc8b9060536199dfcdc058a
AliShahbazzz <alishahbaz617@gmail.com>
2025-08-24 03:23:42 +0530

    Contact us updated

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -29,21 +29,17 @@ export const ContactUs: React.FC = () => {
         <Stack spacing={3} mt={2}>
           <Box display="flex" alignItems="center" gap={2}>
             <Email color="primary" />
-            <Typography variant="body1">support@yourcompany.com</Typography>
+            <Typography variant="body1">management.aliph@gmail.com</Typography>
           </Box>
 
           <Box display="flex" alignItems="center" gap={2}>
             <Phone color="primary" />
-            <Typography variant="body1">+1 (555) 123-4567</Typography>
+            <Typography variant="body1">+91 70934 96194</Typography>
           </Box>
 
           <Box display="flex" alignItems="center" gap={2}>
             <LocationOn color="primary" />
-            <Typography variant="body1">
-              123 Business Street, Suite 456
-              <br />
-              New York, NY 10001
-            </Typography>
+            <Typography variant="body1">Hyderabad, TG 500059</Typography>
           </Box>
         </Stack>
       </Paper>
